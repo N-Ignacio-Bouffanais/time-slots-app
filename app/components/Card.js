8ac05070aa07f976abb691cbd2b9e96a2e6a77db
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-11-21 20:09:07 -0300

    listo el contador, falta la animacion de cambio de Iniciar a Iniciado y de Terminar a Finalizado, ademas de la animacion de no dejar tomar, y quedaria la autenticacion de usuario

--- app/components/Card.js
+++ app/components/Card.js
@@ -2,15 +2,35 @@ import { CountContext } from '../page'
 import { useContext } from 'react'
 
 export default function Card (props) {
+  const num = 0
+  const num2 = 8
   const countContext = useContext(CountContext)
+  console.log(countContext.countState)
   return (
     <div className='card-container'>
       <div className='left-content'>
         <p>{props.time}</p>
       </div>
       <div className='right-content'>
-        <button className='Iniciar button' onClick={() => countContext.countDispatch('increment')}>Iniciar</button>
-        <button className='Terminar button' onClick={() => countContext.countDispatch('decrement')}>Terminar</button>
+        <button
+          className='Iniciar button' onClick={() => {
+            if (countContext.countState !== num) {
+              countContext.countDispatch('decrement')
+            }
+            if (countContext.countState <= 0) {
+              console.log('no hay motos disponibles')
+            }
+          }}
+        >Iniciar
+        </button>
+        <button
+          className='Terminar button' onClick={() => {
+            if (countContext.countState !== num2) {
+              countContext.countDispatch('increment')
+            }
+          }}
+        >Terminar
+        </button>
       </div>
     </div>
   )
